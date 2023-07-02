ee79d75b9efbba0e44d11ba942a0229de86fe5bc
DAMPFERS <razuvaevvladislav060@gmail.com>
2023-07-02 22:00:39 +0700

    Загрузка и выставление хода игры м-у сервером и ESP-32

--- server_version_1.py
+++ server_version_1.py
@@ -4,35 +4,62 @@ import csv_test
 
 LOCALHOST = '192.168.43.98'
 PORT = 1984
+start = False
+download = False
+move = 1
+
+
+def processingCommand(command): 
+    global start
+    if command == 0xff:
+        print("Download ....")
+        start = True
+        return 0
+    if start:
+        if command == 0x01:
+            return 1
+        elif command == 0x02:
+            return 2
+        elif command == 0x03:
+            print("Download completed")
+            global download
+            download = True
+            start = False
+            return 0
+        else:
+            return 0
+            
+    
 
 
 def start_server_one(list_bytes):
-    
     try:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((LOCALHOST, PORT))
         print("ОН ВОССТАЛ !!!")
         count = 0
+        a = 0
         server.listen(4)
         while(True):
             client_socket, address = server.accept()
             data = client_socket.recv(1024).decode('utf-8')
             print(data)
-            if data == '1':
-                count = 1
-            elif data == '2':
-                count = 2
-            elif data == '3':
-                return
-            for i in list_bytes[count]:
-                client_socket.send(i)
+            count = processingCommand(int(data))
+            #client_socket.send("abc".encode('ASCII'))
+            if start:
+                for i in list_bytes[count]:
+                    client_socket.send(i)
+            if download:
+                #global move
+                if a == 0: a = int(input("Command: "))
+                str_move = "S" + str(move)
+                for i in str_move:
+                    client_socket.send(i.encode('ASCII'))
             client_socket.shutdown(socket.SHUT_WR)
     except KeyboardInterrupt:
         server.close()
         print('Finish')
-#def load_page_from_get_request(request_data):
- #   HDRS = 'HTTP/1.1 200 OK\r\nContent_Type: text/html; charset=utf-8\r\n\r\n'
- #   return HDRS.encode('utf-8') + request_data.encode('utf-8')
+
 
 
 
--- sketch_may01a/sketch_may01a.ino
+++ sketch_may01a/sketch_may01a.ino
@@ -1,5 +1,7 @@
 #include <WiFi.h>
 
+#define START 0xff
+
 const char* host = "192.168.43.98";
 //const char* host = "192.168.1.107";
 uint16_t port = 1984;
@@ -7,10 +9,11 @@ const char* ssid     = "Honor 8C";
 const char* password = "89243536319";
 //const char* ssid     = "SumОм";
 //const char* password = "sumomsumom";
-byte feedback = 0;
-
+//byte feedback = 0;
+byte feed_back;
 
 void setup() {
+  feed_back = START;
   Serial.begin(9600);
   WiFi.begin(ssid, password);
   delay(200);
@@ -27,16 +30,20 @@ void loop() {
   WiFiClient client;
   if (!client.connect(host,port))
     delay(3000);
-  if (client.connected())
+  if(client.connected())
   {
-    client.print(feedback);  
+    client.print(feed_back);  
     delay(500);
     if (client.available() > 0){
-      byte packageS[9];
-      int packageS_size = client.readBytes(packageS,9);
-      feedback = packageS[0];
-      for (int i = 0; i < 9; i++)
+      //byte packageS[9];
+      byte count = client.available();
+      byte packageS[count];
+      int packageS_size = client.readBytes(packageS,count);
+      feed_back = packageS[0];
+      for (int i = 0; i < count; i++)
         Serial.write(packageS[i]);
+        //Serial.println(packageS[i],BIN);
+      //Serial.println("---------------------------");
     }
     //char w[15];
     //int count = client.available();
